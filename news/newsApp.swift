c1088925ffc6fa4655a9265aeccc3de1577a2e29
aurelionsulll <mennanamehdi@gmail.com>
2021-08-24 14:44:56 +0100

    Initial Commit

---
+++ news/newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsApp.swift
+//  news
+//
+//  Created by mehdi on 24/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
